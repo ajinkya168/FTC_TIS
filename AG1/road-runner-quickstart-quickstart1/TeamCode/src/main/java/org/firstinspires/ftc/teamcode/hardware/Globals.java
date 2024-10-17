8e12d6377c4303907a519bcfa65b22678b1dac84
Ajinkya <ajinkyagiri28@gmail.com>
2024-10-17 13:21:35 +0530

    First commit -m

---
+++ AG1/road-runner-quickstart-quickstart1/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Globals.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamcode.hardware;
+
+import com.acmerobotics.dashboard.config.Config;
+
+//import org.firstinspires.ftc.teamcode.drive.geometry.Pose;
+
+@Config
+public class Globals {
+
+    public static Location ALLIANCE = Location.RED;
+
+    public static Location SIDE = Location.FAR;
+
+    public static boolean IS_AUTO = false;
+
+
+}
